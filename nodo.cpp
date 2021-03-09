02332ddc76df608f1db2310e1e4d3977f7209330
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-03-09 16:53:00 -0600

    Create nodo.cpp

---
+++ nodo.cpp
@@ -0,0 +1,33 @@
+#include "nodo.h"
+
+template <typename T>
+
+Nodo<T>::Nodo()
+{
+    data = NULL;
+    next = NULL;
+}
+
+template <typename T>
+
+Nodo<T>::Nodo(T data_)
+{
+    data = data_;
+    next = NULL;
+}
+
+template <typename T>
+
+void Nodo<T>::purga()
+{
+    if (next)
+        next->purga();
+    delete this;
+}
+
+template <typename T>
+
+void Nodo<T>::imprime_n()
+{
+    cout << data << "-> ";
+}
