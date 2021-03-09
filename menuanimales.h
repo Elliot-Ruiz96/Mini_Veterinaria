03b07d1d0fa47f814d0548d2c873e456b71574ca
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-03-09 13:21:47 -0600

    Create menuanimales.h

---
+++ menuanimales.h
@@ -0,0 +1,20 @@
+#ifndef MENUANIMALES_H
+#define MENUANIMALES_H
+
+#include "lista.h"
+#include "lista2.h"
+#include "menu.h"
+#include "animal.h"
+#include "animali.h"
+#include "animale.h"
+#include <fstream>
+#include <sstream>
+#include <conio.h>
+
+class MenuAnimales
+{
+public:
+    MenuAnimales();
+};
+
+#endif // MENUANIMALES_H
