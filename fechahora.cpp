d1f70be6f7a4e0a9e40fd6f79755e5c5f2052163
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-03-10 11:12:45 -0600

    Create fechahora.cpp

---
+++ fechahora.cpp
@@ -0,0 +1,18 @@
+#include "fechahora.h"
+
+using namespace std;
+
+FechaHora::FechaHora(int dia, int mes, int anio, int hor, int min, int seg)
+{
+    this->dia = dia;
+    this->mes = mes;
+    this->anio = anio;
+    this->hor = hor;
+    this->min = min;
+    this-> seg = seg;
+}
+
+void FechaHora::muestraFechaHora()
+{
+    cout << " " << this->dia << " " << this->mes << " " << this->anio << endl;
+}
